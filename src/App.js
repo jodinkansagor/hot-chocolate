bbc2eeeeab5cb2fd0128883a94705312efbe1dab
jbj <jkansagor@newrelic.com>
2021-12-18 14:49:07 -0800

    made stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="content">
+        <div className="header">
+
+        <h1>Happy Winter Solstice!</h1>
+        <h2>From JBJ and Dandy</h2>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We wanted to create something sweet that could keep you warm or bring
+          you a moment of joy in this dark time. Share a hot chocolate with a
+          friend and know that soon enough the light will return. We hope you have a love filled winter solstice, 2021. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Recipe</h3>
+        <ol>
+          <li>Add mix to small pot.</li>
+          <li>Add 1/4 cup of whatever milk you want to use.</li>
+          <li>
+            Turn heat up to medium and use a whisk to stir in and dissolve mix.
+          </li>
+          <li>
+            Once mix is dissolved, add 1 &frac34; cup milk (for 2 cups total) and
+            stir.
+          </li>
+          <li>Heat on medium until desired temperature.</li>
+          <li>
+            Top with your favorite hot chocolate toppings like candy canes,
+            marshmellows, cinnamon, a punch of chili, etc!
+          </li>
+        </ol>
+        <h3>Ingredients:</h3>
+        <ul>
+          <li>Equal Exchange cocoa powder</li>
+          <li>Coconut sugar</li>
+          <li>Tiny pinch of salt</li>
+        </ul>
+      </div>
+      <img
+        className="hotChocolate"
+        src="https://c.pxhere.com/images/24/86/0e3c27b462e863a81eb87075a168-1444601.jpg!d"
+        alt="hot chocolate"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
