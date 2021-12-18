11384ff7880acb5971fd59bea2463e11df4081c8
jbj <jkansagor@newrelic.com>
2021-12-18 15:13:08 -0800

    typo

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
       <div className="content">
         <div className="header">
 
-        <h1>Happy Winter Solstice!</h1>
+        <h1>Happy Winter Solstice 2021!</h1>
         <h2>From JBJ and Dandy</h2>
         </div>
         <p>
-          We wanted to create something sweet that could keep you warm or bring
+          We wanted to offer something sweet that could keep you warm or bring
           you a moment of joy in this dark time. Share a hot chocolate with a
-          friend and know that soon enough the light will return. We hope you have a love filled winter solstice, 2021. 
+          friend and know that soon enough the light will return. We hope you have an easy winter and a wonderful new year. 
         </p>
-        <h3>Recipe</h3>
+        <h3>Recipe:</h3>
         <ol>
           <li>Add mix to small pot.</li>
           <li>Add 1/4 cup of whatever milk you want to use.</li>
@@ -28,7 +28,7 @@ function App() {
           <li>Heat on medium until desired temperature.</li>
           <li>
             Top with your favorite hot chocolate toppings like candy canes,
-            marshmellows, cinnamon, a punch of chili, etc!
+            marshmellows, cinnamon, a pinch of chili, etc!
           </li>
         </ol>
         <h3>Ingredients:</h3>
